e01eb3ccfcb7169e83947e4846886958c7af7abd
kdy <laasl@naver.com>
2019-01-24 11:57:07 +0900

    프로젝트를 생성하는 함수 추가

---
+++ bin/addproject.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding:utf8
+import sys
+import os
+
+def addProject(name):
+	"""
+	프로젝트를 생성하는 함수입니다.
+	"""
+	root = "/project"
+	subDirs = ["shot",
+			"in",
+			"out",
+			"edit",
+			"asset",
+			"asset/char",
+			"asset/shader",
+			"config",
+			"config/ocio",
+			"config/thumbnail",
+			"doc",
+			"doc/concept"]
+	for s in subDirs:
+		p = "/".join([root, name,s])
+		os.makedirs(p)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "사용법:"
+		print "$ addproject projectname"
+		sys.exi()
+	name = sys.argv[1]
+	addProject(name)
