1063c96289850a621d0b56f40ca015b65eb355ad
kdy <laasl@naver.com>
2019-02-08 16:42:27 +0900

    add pip.sh

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -3,5 +3,8 @@ cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
-pip install --upgrade setuptools
-pip install pymediainfo
+#pip install --upgrade setuptools #간혹 이작업 이후 부팅이 안됨
+pip install --user pymediainfo
+pip install --user Pyside2
+pip install --user numpy
+pip install --user Cython
