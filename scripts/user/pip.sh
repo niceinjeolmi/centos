5c7dcbb15a10b869b723efcccf67722dac13ca7b
kdy <laasl@naver.com>
2019-02-26 15:52:18 +0900

    pip user수정

--- scripts/user/pip.sh
+++ scripts/user/pip.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-cd /tmp
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-
 #pip install --upgrade setuptools #간혹 이작업 이후 부팅이 안됨
 pip install --user pymediainfo
 pip install --user Pyside2
