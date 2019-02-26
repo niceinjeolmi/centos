42440c6768bbc589715139a2ebc3792ceb0fff0a
kdy <laasl@naver.com>
2019-02-26 15:32:47 +0900

    install수정, 이동, 이름수정

---
+++ scripts/install_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+if [[ $EUID -e 0 ]];then
+./root/nvidia.sh
+./root/utility.sh
+./root/code.sh
+./root/mpv.sh
+./root/djv.sh
+./root/devtools.sh
+./root/handbrake.sh
+./root/openimageio.sh
+./rmbeep.sh
+
+else
+# install #USER
+./user/trans.sh
+./rmbeep.sh
+./user/ocio_config.sh
+./user/ffmpeg.sh
+./user/pip.sh
+
+# gcc6.x를 이용해서 빌드한다
+scl enable devtoolset-6 ./cmake.sh
+scl enable devtoolset-6 ./openexr.sh
+scl enable devtoolset-6 ./opencolorio.sh
+fi
